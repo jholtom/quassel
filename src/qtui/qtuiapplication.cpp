968cb2ac99ad2d48c77d28ed31636d685b39d0f6
Jacob Holtom <jacob@holtom.me>
2019-06-08 18:24:14 -0700

    Added xcodeproj for reference

--- src/qtui/qtuiapplication.cpp
+++ src/qtui/qtuiapplication.cpp
@@ -31,7 +31,7 @@
 #include "types.h"
 
 QtUiApplication::QtUiApplication(int& argc, char** argv)
-    : QApplication(argc, argv)
+    : QApplication(argc, argv)		
 {
 #if QT_VERSION >= 0x050600
     QGuiApplication::setFallbackSessionManagementEnabled(false);
